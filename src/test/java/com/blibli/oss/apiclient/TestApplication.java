e3dc312ba68e1d69d014fe7c974ef1256c114ba2
eko.k.khannedy <eko.k.khannedy@gdn-commerce.com>
2019-12-05 19:55:00 +0700

    create integration test

---
+++ src/test/java/com/blibli/oss/apiclient/TestApplication.java
@@ -0,0 +1,10 @@
+package com.blibli.oss.apiclient;
+
+import com.blibli.oss.apiclient.annotation.EnableApiClient;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableApiClient
+@SpringBootApplication
+public class TestApplication {
+
+}
